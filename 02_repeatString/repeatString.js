8f6d789dfd3d1985b40bbd5c9038bea47f869b20
PedroBettencourt <pedropbettencourt@gmail.com>
2025-03-09 17:48:00 -0100

    Solve the javascript foundations exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(name, times) {
+    let repeatedName = "";
 
+    if (times < 0) {
+        return "ERROR"
+    }
+
+    for (let i = 0; i < times; i++) {
+        repeatedName += name;
+    }
+    return repeatedName;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    newArray = []
+
+    element: for (i of array) {
+        for (j of args) {
+            if (j === i) {
+                continue element;
+            }  
+        }
+        newArray.push(i);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
