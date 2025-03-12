21dd3abe097eefd51acc0b0f140799eef523b013
PedroBettencourt <pedropbettencourt@gmail.com>
2025-03-12 22:48:25 +0000

    Solve exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+filteredLetters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+const palindromes = function (word) {
+    word = word.toLowerCase().split("").filter((a) => filteredLetters.includes(a)).join("")
+    inverseWord = word.split("").reverse().join("");
+    return inverseWord === word;
 };
 
 // Do not edit below this line
