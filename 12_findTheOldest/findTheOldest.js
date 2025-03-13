bbe1a8fc41327683e25493d0d31fb91bb2bd796d
PedroBettencourt <pedropbettencourt@gmail.com>
2025-03-13 01:13:14 +0000

    Solve exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+function getAge(person) {
+    if (!person.yearOfDeath) person.yearOfDeath = (new Date()).getFullYear();
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
-};
+const findTheOldest = function(people) {
+    return people.reduce((oldest, curr) => {
+        const ageCurr = getAge(curr);
+        const ageOldest = getAge(oldest);
+        console.log(ageOldest)
+        return (ageCurr > ageOldest) ? curr : oldest;
+    }
+)};
 
 // Do not edit below this line
 module.exports = findTheOldest;
